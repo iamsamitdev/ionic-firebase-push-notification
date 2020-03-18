c885472d40b8e87ed97893b9260396072a6a72b1
Samit Koyom <samitkoyom@gmail.com>
2020-03-19 01:07:21 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FCM } from "@ionic-native/fcm/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"]
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fcm: FCM
   ) {
     this.initializeApp();
   }
@@ -22,6 +24,32 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      // subscribe to a topic
+      // this.fcm.subscribeToTopic('Deals');
+
+      // get FCM token
+      this.fcm.getToken().then(token => {
+        console.log(token);
+      });
+
+      // ionic push notification example
+      this.fcm.onNotification().subscribe(data => {
+        console.log(data);
+        if (data.wasTapped) {
+          console.log("Received in background");
+        } else {
+          console.log("Received in foreground");
+        }
+      });
+
+      // refresh the FCM token
+      this.fcm.onTokenRefresh().subscribe(token => {
+        console.log(token);
+      });
+
+      // unsubscribe from a topic
+      // this.fcm.unsubscribeFromTopic('offers');
     });
   }
 }
